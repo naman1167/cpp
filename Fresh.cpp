db1614db5c29bb04bd76248792a56843783a3490
Naman Sethi <162817546+naman1167@users.noreply.github.com>
2025-01-17 01:02:55 +0530

    Add files via upload

---
+++ Fresh.cpp
@@ -0,0 +1,71 @@
+// #include<iostream>
+// using namespace std ;
+// int main () 
+
+// {
+// // {
+// // int arr[20]= {10,20,30,40,50,45,78,90,9,23,56,77,8,9,1,2,1909,34,7,987} ;
+
+// // cout<<arr[0]<<endl ;
+// // cout<<arr[1]<<endl ;
+// // cout<<arr[2]<<endl ;
+// // cout<<arr[3]<<endl ;
+// // cout<<arr[4]<<endl ;
+// // cout<<arr[5]<<endl ;
+// // cout<<arr[6]<<endl ;
+// // cout<<arr[7]<<endl ;
+// // cout<<arr[8]<<endl ;
+// // cout<<arr[9]<<endl ;
+// // cout<<arr[10]<<endl ;
+// // cout<<arr[11]<<endl ;
+// // cout<<arr[12]<<endl ;
+// // cout<<arr[13]<<endl ;
+// // cout<<arr[14]<<endl ;
+// // cout<<arr[15]<<endl ;
+// // cout<<arr[16]<<endl ;
+// // cout<<arr[17]<<endl ;
+// // cout<<arr[18]<<endl ;
+// // cout<<arr[19]<<endl ;
+// // cout<<arr[20]<<endl ;
+
+
+
+
+
+
+// // #include<iostream>
+// // using namespace std;
+// // int main()
+// // {
+// //     int myArray[20] = {10, 20, 30, 40, 50};
+
+// //     cout << myArray[0] << endl;
+// //     cout << myArray[1] << endl;
+// //     cout << myArray[2] << endl;
+// //     cout << myArray[3] << endl;
+// //     cout << myArray[4] << endl;
+
+// //     return 0;
+// // }
+// {
+//     int test [3][3]={1,1,2,2,3,3};
+//     cout<<test[0][0]<<" ";
+//     cout<<test[0][1]<<" ";
+//     cout<<test[0][2]<<endl;
+//     cout<<test[1][0]<<" ";
+//     cout<<test[1][1]<<" ";
+//     cout<<test[1][2]<<endl;
+//     cout<<test[2][0]<<" ";
+//     cout<<test[2][1]<<" ";
+//     cout<<test[2][2]<<endl;
+
+//     return 0 ;
+
+// }
+
+
+
+// }
+
+
+
---
+++ Strings.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+using namespace std ;
+int main ()
+
+{
+    string str1= "Hello World! ";
+    string str2 = "C++ Programming is Fun";
+    string str3 ;
+    cout<<"Length of str1: "<<str1.length()<<endl;
+    
+
+return 0 ;
+
+}
