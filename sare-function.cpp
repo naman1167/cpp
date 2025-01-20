3b8b08008bb69430922467c5e1a668a7984fccc9
Naman Sethi <162817546+naman1167@users.noreply.github.com>
2025-01-20 19:46:56 +0530

    Add files via upload

---
+++ sare-function.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+// Hello World !!1
+
+// void hello ();
+// void hello (){
+//     cout<<"Hello World!!!";
+// }
+// int main (){
+//     hello();
+// }
+
+//  Swapping Variables
+
+
+// void Swapp ();
+// void Swapp (){
+    
+// int main  ();
+//     int a = 10 ;
+//     int b = 5 ;
+//     a = a+b ;
+//     b = a- b ;
+//     a = a -b ;
+//     cout<<" Value of a: "<<a<<endl; 
+//     cout<<"Value of b:"<<b<<endl;
+
+// }
+// int main (){
+//     Swapp();
+// }
+
+
+// 
+
+// Addition of Numbers
+
+// void ADD();
+// void ADD(){
+    
+//     int main ();
+//     int a = 10 ;
+//     int b = 20 ;
+//     cout<<"The Value of 10 + 20 is : "<<a+b;
+
+// }
+// int main (){
+//     ADD();
+// }
+
+
+//  Multiplication
+
+// void Multiply();
+// void Multiply(){
+
+//     int main ();
+//     int a = 10 ;
+//     int b = 2 ;
+//     cout<<"10 Multiplied by 2 is  "<<a*b;
+// }
+
+// int main (){
+//     Multiply();
+// }
+
+
+
+// 
