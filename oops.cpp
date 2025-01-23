e2ae784d4c8da565682bc0d9eb915af951d6d5d5
Naman Sethi <162817546+naman1167@users.noreply.github.com>
2025-01-23 10:05:18 +0530

    Add files via upload

---
+++ oops.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std ;
+class demo ; {
+    public :
+    int x = 10;
+    void display (){
+        cout<<"The value of x is : "<<x<<endl;
+    }
+
+
+}
+
+int main ()
+{
+    demo.d
+    display () ;
+
+
+return 0 ;
+}
+
+
