15a0dc7db3f78a2528dfdc0f4086d6db8ba1a6d9
Naman Sethi <162817546+naman1167@users.noreply.github.com>
2025-01-26 19:56:48 +0530

    Add files via upload

---
+++ Question-7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std ;
+
+
+
+int main (){
+    int Arr [10] = {1,2,3,4,5,6,7,8,9,10};
+    cout<<"The first Natural  Number is "<<Arr[0]<<endl;
+    cout<<"The Second Natural  Number is "<<Arr[1]<<endl;
+    cout<<"The Third Natural  Number is "<<Arr[2]<<endl;
+    cout<<"The Fourth Natural  Number is "<<Arr[3]<<endl;
+    cout<<"The Fifith Natural  Number is "<<Arr[4]<<endl;
+    cout<<"The Sixth Natural  Number is "<<Arr[5]<<endl;
+    cout<<"The Sevnth Natural  Number is "<<Arr[6]<<endl;
+    cout<<"The Eighth Natural  Number is "<<Arr[7]<<endl;
+    cout<<"The Nineth Natural  Number is "<<Arr[8]<<endl;
+    cout<<"The Tenth Natural  Number is "<<Arr[9]<<endl;
+
+    
+    return 0 ;
+}
---
+++ Question-8.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ;
+
+
+int main (){
+    int My_arr [5] ={1,2,3,4,5};
+    cout<<"the reverse of this array is "<<My_arr<<endl;
+
+
+
+
+
+}
