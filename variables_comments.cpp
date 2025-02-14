c0a904a7b7f4137ffebbfb29b41d93a5cddda96c
Naman Sethi <162817546+naman1167@users.noreply.github.com>
2025-02-14 15:31:46 +0530

    Add files via upload

---
+++ variables_comments.cpp
@@ -0,0 +1,17 @@
+//  variables and Comments 
+
+//  variabless 
+/*variables are just container to store data */
+
+#include <iostream>
+using namespace std ;
+
+int main (){
+    int sum = 6 ; // it is a variable of name sum
+    int sum1= 7 ;
+   cout<<"the sum of 6 and 7 is "<<sum+sum1;
+    return 0 ;
+
+}
+
+
